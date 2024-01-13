cdb622f54e2d53936ee8f8d09f696484f21235de
ankitrosh <roshantalluri@gmail.com>
2024-01-13 22:47:20 +0530

    adds auth amd routes

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+import { SignUp } from "@clerk/nextjs"
+const SignUP = () => {
+  return <SignUp />
+}
+
+export default SignUP
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image"
-
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Next14, here I come.</h1>
-    </div>
-  )
-}
